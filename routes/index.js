ac04e55e4d1fe65102940649a119cabb8db2a129
Ingo Bezerra Wolter <ibwolter@sfiec.org.br>
2018-10-02 17:06:20 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Listagem de Clientes',
+    message: 'Clientes já cadastrados no sistema',
+   });
+});
+
+router.get('/new', function(req, res, next){
+  res.render('new',{title: "Cadastro de Cliente", action:"/new"})
+})
+
+router.post('/new', function(req, res, next){
+  res.redirect('/?new=true')
+})
+
+module.exports = router;
